450dfbbf9015062b46f1a635c7db46519d0f9165
amarantineblu <amaranthblu@yahoo.com>
2026-04-06 13:07:03 -0500

    addition of listile to home page showing calendar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,23 +14,18 @@ class MyHomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         // backgroundColor:  ,
-        body: Container(
+        appBar: AppBar(
+          backgroundColor: TColors.primaryColor,
+          leading: Icon(Icons.menu),
+          title: Text('Berlin, Germany'),
+          actions: [Icon(Icons.search)],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(8.0),
           child: Column(
             // mainAxisAlignment: MainAxisAlignment.center,
             // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.menu),
-                  Text(
-                    'Berlin, Germany',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  Icon(Icons.search),
-                ],
-              ),
-              SizedBox(height: 20),
               SizedBox(
                 height: 180,
                 child: ListView.builder(
@@ -41,89 +36,162 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(
-                      'News Weather Details',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                      textAlign: TextAlign.left,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'News Weather Details',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                    textAlign: TextAlign.left,
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
                     ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      color: TColors.primaryColor,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(16),
-                                topRight: Radius.circular(16),
-                              ),
-                              image: DecorationImage(
-                                image: AssetImage(
-                                  "images/tropical_storm.jpeg",
-                                ), // your header image
-                                fit: BoxFit.cover,
-                              ),
+                    color: TColors.primaryColor,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(16),
+                              topRight: Radius.circular(16),
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                "images/tropical_storm.jpeg",
+                              ), // your header image
+                              fit: BoxFit.cover,
                             ),
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                child: Column(
-                                  children: [
-                                    Icon(Icons.water_drop, color: Colors.white),
-                                    Text(
-                                      'Humidity',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium
-                                          ?.copyWith(color: Colors.white),
-                                    ),
-                                    Text(
-                                      '80%',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium
-                                          ?.copyWith(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(Icons.water_drop, color: Colors.white),
+                                  Text(
+                                    'Humidity',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                  Text(
+                                    '80%',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                ],
                               ),
-                              Container(
-                                child: Column(
-                                  children: [
-                                    Icon(Icons.air, color: Colors.white),
-                                    Text(
-                                      'Wind Speed',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium
-                                          ?.copyWith(color: Colors.white),
-                                    ),
-                                    Text(
-                                      '15 km/h',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium
-                                          ?.copyWith(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(Icons.air, color: Colors.white),
+                                  Text(
+                                    'Wind Speed',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                  Text(
+                                    '15 km/h',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Calendar',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  // ignore: sized_box_for_whitespace
+                  Container(
+                    // height: 100,
+                    width: double.infinity,
+                    // color: TColors.backgroundColor,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          tileColor: TColors.primaryColor,
+                          leading: Icon(
+                            Icons.calendar_today,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Monday, 12th June',
+                            style: Theme.of(context).textTheme.bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'Sunny with a chance of rain',
+                            style: Theme.of(context).textTheme.bodySmall
+                                ?.copyWith(color: Colors.white70),
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        ListTile(
+                          tileColor: TColors.primaryColor,
+                          leading: Icon(
+                            Icons.calendar_today,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Tuesday, 13th June',
+                            style: Theme.of(context).textTheme.bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'Cloudy with light showers',
+                            style: Theme.of(context).textTheme.bodySmall
+                                ?.copyWith(color: Colors.white70),
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        ListTile(
+                          tileColor: TColors.primaryColor,
+                          leading: Icon(
+                            Icons.calendar_today,
+                            color: Colors.white,
+                          ),
+                          title: Text(
+                            'Wednesday, 14th June',
+                            style: Theme.of(context).textTheme.bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'Partly cloudy with a chance of thunderstorms',
+                            style: Theme.of(context).textTheme.bodySmall
+                                ?.copyWith(color: Colors.white70),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
